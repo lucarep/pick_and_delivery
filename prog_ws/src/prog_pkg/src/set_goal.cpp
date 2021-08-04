2d41098aa629af87147a5bf1c015826961185c31
lucarep <repechini.1806773@studenti.uniroma1.it>
2021-08-04 15:54:15 +0200

    Aggiunti commenti utili

--- prog_ws/src/prog_pkg/src/set_goal.cpp
+++ prog_ws/src/prog_pkg/src/set_goal.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include <vector>
+#include <stdio.h>
 #include "std_msgs/String.h"
 #include "prog_pkg/Goal.h"
 #include "geometry_msgs/PoseStamped.h"
@@ -10,6 +11,8 @@
 #include "geometry_msgs/TransformStamped.h"
 #include "nav_msgs/Odometry.h"
 #include "sensor_msgs/LaserScan.h"
+
+// Dichiarazione variabili globali
 
 std::vector<float> target_position(2,0);
 std::vector<float> old_position(2,0);
@@ -23,10 +26,22 @@ int message_published = 0;
 int cruising = 0;
 float T = 1;
 
+/*
+* Funzione che prende in input un messaggio con le coordinate
+* e setta un nuovo goal
+*/
+
 void SetGoal_Callback(const prog_pkg::Goal& new_goal){
+
+    /*
+    * Per settare una posizione che non faccia crashare il programma
+    * usare la funzione 2D Nav Goal di rviz e stampare le coordinate con echo
+    */
 
     new_goal_msg.header.seq = n;
     n++;
+
+    // Comunicazione del tempo corrente
 
     new_goal_msg.header.stamp = ros::Time::now();
     new_goal_msg.header.frame_id = "map";
@@ -35,20 +50,37 @@ void SetGoal_Callback(const prog_pkg::Goal& new_goal){
     new_goal_msg.pose.position.y = new_goal.y;
     new_goal_msg.pose.position.z = 0;
 
+    /*
+    * x = 53.88 
+    * y = 18.0
+    * theta = 0.02
+    */
+
     new_goal_msg.pose.orientation.x = 0;
     new_goal_msg.pose.orientation.y = 0;
     new_goal_msg.pose.orientation.z = 0;
     new_goal_msg.pose.orientation.w = new_goal.theta;
 
+    // Per chiamare la callback solo una volta
+
     message_published = 1;
+
+    // Inizio lo spostamento
+
     cruising = 1;
 
-    // Save the goal position
+    // Salvare le coordinate del goal
+
     target_position[0] = new_goal_msg.pose.position.x;
     target_position[1] = new_goal_msg.pose.position.y;
 
-    // pub.publish(new_goal_msg);
 }
+
+/*
+* Funzione che verifica la posizione del robot verificando se posso
+* trasformare dal frame della mappa al base_link, se affermativo, 
+* prendo la trasformata e la metto dentro transformStamped
+*/
 
 void position_CallBack(const tf2_msgs::TFMessage& tf)
 {
@@ -60,6 +92,8 @@ void position_CallBack(const tf2_msgs::TFMessage& tf)
         geometry_msgs::TransformStamped transfromStamped;
         transfromStamped = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
 
+        // Aggiorno la posizione corretta
+
         current_position[0] = transfromStamped.transform.translation.x;
         current_position[1] = transfromStamped.transform.translation.y;
 
@@ -69,11 +103,18 @@ void position_CallBack(const tf2_msgs::TFMessage& tf)
 
 void check1_CallBack(const ros::TimerEvent& event)
 {
+    // eseguo il controllo solo se mi muovo
+
     if (cruising != 0)
     {
         ROS_INFO("Check if i'm moving");
         float distance;
         distance = sqrt(pow(current_position[0] - old_position[0],2) + pow(current_position[1] - old_position[1],2));
+
+        /*
+        * Se non mi sono spostato di una certa treshold  
+        * allora comunico che mi sono bloccato
+        */
 
         if (distance < 0.8)
         {
@@ -90,6 +131,8 @@ void check1_CallBack(const ros::TimerEvent& event)
 
 void check2_CallBack(const ros::TimerEvent& event)
 {
+    // eseguo il controllo solo se mi muovo
+
     if (cruising != 0)
     {
         ROS_INFO("Checking if it has been passed too much time...");
@@ -105,6 +148,14 @@ void check2_CallBack(const ros::TimerEvent& event)
 }
 
 int main(int argc, char **argv){
+
+    std::cout << R"(
+   _____      __     ______            __
+  / ___/___  / /_   / ____/___  ____ _/ /
+  \__ \/ _ \/ __/  / / __/ __ \/ __ `/ / 
+ ___/ /  __/ /_   / /_/ / /_/ / /_/ / /  
+/____/\___/\__/   \____/\____/\__,_/_/                                            
+)" << '\n';
     
     ros::init(argc,argv,"set_goal");
 
@@ -118,12 +169,17 @@ int main(int argc, char **argv){
     ros::Subscriber sub = n.subscribe("New_Goal",1000,SetGoal_Callback);
     ros::Subscriber sub_tf = n.subscribe("tf",1000,position_CallBack);
 
+    /* Controllo con dei tempi prefissati lo stato della navigazione
+    *  del robot
+    */
+
     ros::Timer timer1 = n.createTimer(ros::Duration(0.5), check1_CallBack);
     ros::Timer timer2 = n.createTimer(ros::Duration(50), check2_CallBack);
 
     int count = 0;
     while (ros::ok())
     {
+        // Viene inviato un solo messaggio con le coordinate
         if(message_published != 0)
         {
             ROS_INFO("Publishing a new goal position");
